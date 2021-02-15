97656311027125b6418263c6fb33d0ca919ffa20
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-15 20:48:06 +0100

    coat

--- coats.js
+++ coats.js
@@ -1,15 +1,15 @@
 let isRainy = true;
 let isWindy = false;
 let isSun = true;
-let isSummerTemperature = 35;
-let isWinterTemperature = 29;
+let isSummerTemperature = 28;
+let isWinterTemperature = 9;
 
 if (isRainy && isWindy) {
   console.log('Mettez votre manteau')
 }else if(isSummerTemperature >= 30 && isWindy){
     console.log('Mettez un short')
 }
-else if(isWinterTemperature <= 14 && isRainy){
+else if(isWinterTemperature >= 14 && isRainy){
     console.log('Mettez Votre manteau et prenez votre parapluie')
 }
     else if(isSummerTemperature >= 25 && isRainy){
