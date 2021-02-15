90365164ee2a4e064ca00517c507abd545d76b99
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-15 16:48:16 +0100

    Trading

---
+++ trading.js
@@ -0,0 +1,20 @@
+let Pricebitcoin = 46000 ;
+
+
+
+
+if(Pricebitcoin <= 40000){
+    console.log("acheter Bitcoin")
+}else if(Pricebitcoin <= 50000){
+    console.log("Maintenir le prix")
+}
+else{
+    console.log("vendre le bitcoin")
+}
+
+
+
+
+
+
+
