69f8bf2505bd4f22744d0aa4a8832dfa95cb4867
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-15 22:48:37 +0100

    xorJs

---
+++ xor.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 10;
+let c = 15;
+let d = 6;
+
+
+if (a ^ b) {
+    console.log("XOR => False")
+} else if (c ^ d) {
+    console.log("XOR => True")
+} else {
+    console.log("Error 404")
+};
