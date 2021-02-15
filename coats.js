a34aa26bff6c7779b0b62333cbd625370bbad467
adouch9 <mr.ouldmoussa@gmail.com>
2021-02-15 21:07:14 +0100

    coat

--- coats.js
+++ coats.js
@@ -1,19 +1,20 @@
-let isRainy = true;
+let isRainy = false;
 let isWindy = false;
-let isSun = true;
 let isSummerTemperature = 28;
 let isWinterTemperature = 9;
+let youAreInCyprusInAugust = !false;  // Généralement, le long des côtes de Chypre, il ne pleut jamais pendant une longue période, de juin 
+                                       //   à septembre, et le soleil brille presque toujours.
 
 if (isRainy && isWindy) {
   console.log('Mettez votre manteau')
 }else if(isSummerTemperature >= 30 && isWindy){
     console.log('Mettez un short')
 }
-else if(isWinterTemperature >= 14 && isRainy){
+else if(isWinterTemperature >= 4 && isRainy){
     console.log('Mettez Votre manteau et prenez votre parapluie')
 }
-    else if(isSummerTemperature >= 25 && isRainy){
-        console.log('Bienvenue a Paris lol')
+    else if(isSummerTemperature >= 25 && youAreInCyprusInAugust){
+        console.log('Tu ne trouvera aucun parapluie en vente lol')
 }
 else {
    console.log('Consulter la meteo pour savoir comment shabiller')
